bc76d9af130c7e5d0766feb8a8d2b470b6f47f30
Shohan <shohan.hasan.x6@gmail.com>
2013-07-25 19:54:48 -0400

    cuttingsticks accepted

--- CuttingSticks.java
+++ CuttingSticks.java
@@ -2,31 +2,36 @@ import java.util.*;
 public class CuttingSticks {
 
 	static int[][] cutCosts = new int[52][52]; //memo table
-	static int[] cutLocs;
-	public static void Main(String[] args){
+	static int[] cutLocs = new int[52];
+	
+	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		
 		while(in.hasNext()){
 			int length = in.nextInt();
 			if(length==0)
 				break;
-			Arrays.fill(cutCosts, -1); //reset memo table
-	
+			for(int[] arr : cutCosts){
+				Arrays.fill(arr, -1); //reset memo table
+			}
 			
-			cutLocs = new int[length];
+			//cutLocs = new int[length];
+			Arrays.fill(cutLocs,0); // we use zero as "first cut point"
 			int cuts = in.nextInt();
 			for(int i=1; i<=cuts; i++){ // loads cuts into range 1:cuts
 				cutLocs[i] = in.nextInt();
 			}
-			int cost = solve(0,length);
-			System.out.println(cost);
+			cutLocs[cuts+1]=length;//end point is "last cut point"
+			
+			int cost = solve(0,cuts+1);
+			System.out.printf("The minimum cutting is %d.\n", cost);
 			
 		}
 	}
 	
 	static int solve(int begin, int end){
 		/* Need base case.*/
-		if(begin>=end)
+		if(begin+1==end)
 			return 0;
 		int left = cutLocs[begin];
 		int right = cutLocs[end];
@@ -34,9 +39,10 @@ public class CuttingSticks {
 		// Compute minimum cost if not found in memoization table
 		if(cutCosts[begin][end]==-1){
 			int minCost = Integer.MAX_VALUE/2;
-			for(int j=begin; j<end-1; j++){
-				minCost = Math.min(minCost, bsize+solve(begin,j)+solve(j+1,end));
+			for(int j=begin+1; j<end; j++){
+				minCost = Math.min(minCost, bsize+solve(begin,j)+solve(j,end));
 			}
+			cutCosts[begin][end] = minCost;
 		}
 		return cutCosts[begin][end];
 	}
