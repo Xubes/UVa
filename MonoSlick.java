35b66e705442b2366b685aab2155d16e03183589
Shohan <shohan.hasan.x6@gmail.com>
2013-08-21 17:20:10 -0400

    New AC solution to Monocycle
    A solver for Monocycle that does not build an explicit state-space
    graph.

--- MonoSlick.java
+++ MonoSlick.java
@@ -1,12 +1,10 @@
+/* Solves Monocycle problem without building an explicit graph. */
 import java.io.*;
 import java.util.*;
 public class MonoSlick {
 
-	//lsb->msb : 2bits for dir, 2 bits for color, 5 bits for row and col each
-	static final int DIR_MASK = 0x3,
-						CLR_MASK = 0x3<<2,
-						COL_MASK = 0x1F<<4,
-						ROW_MASK = 0x1F<<9;
+	//lsb->msb : 2bits for dir, 3 bits for color, 5 bits for row and col each
+	static final int DM = 3, CM = 7, RCM = 31;
 	static final int north = 0, east = 1, south = 2, west = 3, dirs = 4;
 	static final int green = 0, colors=5;
 	static final int[][] dvecs = {{-1,0},{0,1},{1,0},{0,-1}}; // the direction vectors for our 4 directions
@@ -22,6 +20,8 @@ public class MonoSlick {
 			String[] toks = nl.trim().split(" ");
 			rows = Integer.parseInt(toks[0]);
 			columns = Integer.parseInt(toks[1]);
+			if(rows==0 && columns==0)
+				break;
 			
 			int start=0, end=0; //stores the start and end states
 			/* Init grid and parse int the input. */
@@ -33,13 +33,14 @@ public class MonoSlick {
 						start = encode(i,j,green,north);
 					}
 					else if(grid[i][j] == 'T'){
-						end = encode(i,j,green,north); // note the direction is irrelevant for target
+						end = encode(i,j,green,0); // note the direction is irrelevant for target
 					}
 				}
 			}
 			
 			int solution = bfs(start,end);
-			System.out.printf("Case #%d:\n", set++);
+			if(set>1) System.out.println();
+			System.out.printf("Case #%d\n", set++);
 			if(solution!=-1){
 				System.out.printf("minimum time = %d sec\n", solution);
 			}
@@ -61,24 +62,23 @@ public class MonoSlick {
 	 */
 	public static int bfs(int s, int t){
 		TreeSet<Integer> visited = new TreeSet<Integer>(); // set of visited states so we don't double back
-		LinkedList<Integer> q = new LinkedList<Integer>(); // our bfs queue
-		q.add(s);
-		
-		// decode the target state:
-		int tr = getRow(t), tc = getCol(t), tclr = getClr(t);
+		LinkedList<Integer> q1 = new LinkedList<Integer>(); // our bfs queue
+		LinkedList<Integer> q2 = new LinkedList<Integer>();
+		q1.add(s);
 		
 		int dist = 0;
-		while(!q.isEmpty()){
-			LinkedList<Integer> q2 = new LinkedList<Integer>();
+		while(!q1.isEmpty()){
 
-			for(int node : q){
+			for(int node : q1){
 				// decode the state
-				int r = getRow(node);
-				int c = getCol(node);
-				int clr = getClr(node);
-				int dir = getDir(node);
+				int dir = node&DM;
+				int clr = (node>>>2)&CM;
+				int c = (node>>>5)&RCM;
+				int r = (node>>>10)&RCM;
 				
-				if(r==tr && c==tc && clr==tclr)		return dist;
+				// check if this is a target state
+				if((node&(~DM)) == t)		return dist;
+				
 				
 				int leftTurn = encode(r,c,clr,(dir+3)%dirs);	// node for a left turn
 				if(!visited.contains(leftTurn)){
@@ -102,7 +102,9 @@ public class MonoSlick {
 					}
 				}
 			}
-			q = q2;
+			// swap q1 and q2
+			q1 = new LinkedList<Integer>(q2);
+			q2.clear();
 			dist++;
 		}
 		return -1;
@@ -110,26 +112,11 @@ public class MonoSlick {
 	
 	// returns the state properties into encoded into an int
 	public static int encode(int row, int col, int color, int dir){
-		return (row<<9) | (col<<4) | (color<<2) | (dir);
+		return (row<<10) | (col<<5) | (color<<2) | (dir);
 	}
 	
-	// get the row of an encoded state
-	public static int getRow(int state){
-		return (state&ROW_MASK)>>>9;
-	}
-	
-	// get the column of an encoded state
-	public static int getCol(int state){
-		return (state&COL_MASK)>>>4;
-	}
-	
-	// get the color of an encoded state
-	public static int getClr(int state){
-		return (state&CLR_MASK)>>>2;
-	}
-	
-	// get the direction of an encoded state
-	public static int getDir(int state){
-		return state&DIR_MASK;
+	// decoding routine for debugging
+	public static String decode(int state){
+		return "(" + ((state>>>10)&RCM) + "," + ((state>>>5)&RCM) + "," + ((state>>2)&CM) + "," + (state&DM) + ")";
 	}
 }
